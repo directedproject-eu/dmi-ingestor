d0165a5856ee727a2827ca9bce54f9c77926d329
EHJ-52n <e.h.juerrens@52north.org>
2025-06-30 15:41:22 +0200

    Ruff code formatting + linting

--- dmi_ingestor/ingestor.py
+++ dmi_ingestor/ingestor.py
@@ -13,15 +13,11 @@ from osgeo import gdal
 from requests.exceptions import HTTPError
 from s3fs import S3FileSystem
 
-
 # Necessary to avoid writing checksums into the COGs when uploading them to the bucket (cf. https://github.com/boto/boto3/issues/4435)
 os.environ["AWS_REQUEST_CHECKSUM_CALCULATION"] = "when_required"
 os.environ["AWS_RESPONSE_CHECKSUM_VALIDATION"] = "when_required"
 
-logging.basicConfig(
-    level=logging.INFO,
-    stream=sys.stdout
-)
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 logger = logging.getLogger(__name__)
 
 # see https://opendatadocs.dmi.govcloud.dk/Data/Forecast_Data_Weather_Model_HARMONIE_DINI_IG
@@ -63,13 +59,10 @@ CS[Cartesian,2],
         ORDER[2],
         LENGTHUNIT["Metre",1]]]"""
 
+
 def delete_outdated_forecasts(bucket_path, endpoint_url, key, secret):
     logger.debug(f"Delete path {bucket_path} recursively.")
-    s3 = S3FileSystem(
-        endpoint_url=endpoint_url,
-        key=key,
-        secret=secret
-    )
+    s3 = S3FileSystem(endpoint_url=endpoint_url, key=key, secret=secret)
     try:
         s3.rm(bucket_path, recursive=True)
     except FileNotFoundError as err:
@@ -78,7 +71,9 @@ def delete_outdated_forecasts(bucket_path, endpoint_url, key, secret):
 
 def netcdf_to_cog(input_file, output_file):
     dataset = gdal.Open(input_file)
-    options = gdal.TranslateOptions(format="COG", creationOptions=["COMPRESS=LZW"], outputSRS="EPSG:4326")
+    options = gdal.TranslateOptions(
+        format="COG", creationOptions=["COMPRESS=LZW"], outputSRS="EPSG:4326"
+    )
     gdal.Translate(output_file, dataset, options=options)
     dataset = None
 
@@ -90,18 +85,36 @@ def reproject_from_lambert(xarray_dataset, crs_to="epsg:4326"):
     return xarray_dataset
 
 
-def transform_cog_to_single_bands_and_upload_to_bucket(cog_file, folder_name, times, bucket_name=None, bucket_path=None,
-                                                       endpoint=None, key=None, secret=None, upload=False):
+def transform_cog_to_single_bands_and_upload_to_bucket(
+    cog_file,
+    folder_name,
+    times,
+    bucket_name=None,
+    bucket_path=None,
+    endpoint=None,
+    key=None,
+    secret=None,
+    upload=False,
+):
     dataset = gdal.Open(cog_file)
     forecasts = {}
     for band, t in enumerate(times):
-        time_str = str(t).split('.')[0].replace("-", "").replace(":", "")
+        time_str = str(t).split(".")[0].replace("-", "").replace(":", "")
         output_file = os.path.join(folder_name, time_str + ".tif")
-        gdal.Translate(output_file, dataset, bandList=[band+1], options=["BIGTIFF=YES"])
+        gdal.Translate(
+            output_file, dataset, bandList=[band + 1], options=["BIGTIFF=YES"]
+        )
         if upload:
-            upload_to_bucket(output_file, f"{bucket_name}/{bucket_path}/{os.path.basename(output_file)}",
-                             endpoint, key, secret)
-            forecasts[time_str] = f"https://{bucket_name}.{endpoint.lstrip('https://')}/{bucket_path}/{os.path.basename(output_file)}"
+            upload_to_bucket(
+                output_file,
+                f"{bucket_name}/{bucket_path}/{os.path.basename(output_file)}",
+                endpoint,
+                key,
+                secret,
+            )
+            forecasts[time_str] = (
+                f"https://{bucket_name}.{endpoint.lstrip('https://')}/{bucket_path}/{os.path.basename(output_file)}"
+            )
             if band % 10 == 0:
                 logger.info(f"Uploaded {band} of {len(times)} files.")
     dataset = None
@@ -110,13 +123,9 @@ def transform_cog_to_single_bands_and_upload_to_bucket(cog_file, folder_name, ti
 
 def upload_to_bucket(local_file, bucket_file, endpoint_url, key, secret):
     logger.debug(f"Try to upload {local_file} to {bucket_file}.")
-    s3 = S3FileSystem(
-        endpoint_url=endpoint_url,
-        key=key,
-        secret=secret
-    )
-    with open(local_file, 'rb') as f:
-        with s3.open(bucket_file, 'wb') as s3_file:
+    s3 = S3FileSystem(endpoint_url=endpoint_url, key=key, secret=secret)
+    with open(local_file, "rb") as f:
+        with s3.open(bucket_file, "wb") as s3_file:
             s3_file.write(f.read())
             logger.debug("Upload succeeded.")
 
@@ -125,7 +134,9 @@ if __name__ == "__main__":
     logger.info("Start ingesting DMI data.")
     # Configurable parameters
     dmi_api_key = os.getenv("DMI_API_KEY")
-    bucket_endpoint = os.getenv("BUCKET_ENDPOINT", "https://obs.eu-de.otc.t-systems.com")
+    bucket_endpoint = os.getenv(
+        "BUCKET_ENDPOINT", "https://obs.eu-de.otc.t-systems.com"
+    )
     bucket_name = os.getenv("BUCKET_NAME")
     bucket_base_path = os.getenv("BUCKET_BASE_PATH", "data/dmi/forecasts")
     bucket_key = os.getenv("BUCKET_KEY")
@@ -145,7 +156,9 @@ if __name__ == "__main__":
     forecast_json_filename = os.path.join(base_data_dir, "forecasts.json")
 
     for parameter in parameters.split(","):
-        logger.info(f"Start ingesting data from collection '{collection}' for parameter '{parameter}'.")
+        logger.info(
+            f"Start ingesting data from collection '{collection}' for parameter '{parameter}'."
+        )
         bucket_path = f"{bucket_base_path}/{collection}/{parameter}"
         bucket_path_full = f"{bucket_name}/{bucket_path}"
         forecast_json_bucket_path = bucket_path_full + "/forecasts.json"
@@ -153,8 +166,8 @@ if __name__ == "__main__":
             os.makedirs(base_data_dir)
 
         Parts = namedtuple(
-            typename='Components',
-            field_names=['scheme', 'netloc', 'path', 'query', 'fragment']
+            typename="Components",
+            field_names=["scheme", "netloc", "path", "query", "fragment"],
         )
 
         if collection.startswith("harmonie"):
@@ -167,7 +180,7 @@ if __name__ == "__main__":
             "crs": crs,
             "parameter-name": parameter,
             "bbox": bbox,
-            "f": out_format
+            "f": out_format,
         }
 
         url = urlunsplit(
@@ -176,32 +189,49 @@ if __name__ == "__main__":
                 netloc=netloc,
                 path=f"{url_base_path}/{collection}/{request_type}",
                 query=urlencode(query_params),
-                fragment=''
+                fragment="",
             )
         )
         try:
-            logger.info(f"Request data from DMI API.")
+            logger.info("Request data from DMI API.")
             response = requests.get(url)
             response.raise_for_status()
         except HTTPError as err:
             logger.error(err)
         else:
-            delete_outdated_forecasts(bucket_path_full, bucket_endpoint, bucket_key, bucket_secret)
+            delete_outdated_forecasts(
+                bucket_path_full, bucket_endpoint, bucket_key, bucket_secret
+            )
             ds = xarray.open_dataset(BytesIO(response.content))
             if collection.startswith("harmonie"):
                 ds = reproject_from_lambert(ds)
-            logger.info(f"Save data to NetCDF.")
+            logger.info("Save data to NetCDF.")
             ds.to_netcdf(nc_filename)
-            logger.info(f"Transform NetCDF to COG.")
+            logger.info("Transform NetCDF to COG.")
             netcdf_to_cog(nc_filename, cog_filename)
-            logger.info(f"Split COG into bands (time slices) and upload them to bucket.")
+            logger.info(
+                "Split COG into bands (time slices) and upload them to bucket."
+            )
             data = transform_cog_to_single_bands_and_upload_to_bucket(
-                cog_filename, base_data_dir, ds.time.values, bucket_name, bucket_path, bucket_endpoint, bucket_key,
-                bucket_secret, upload)
+                cog_filename,
+                base_data_dir,
+                ds.time.values,
+                bucket_name,
+                bucket_path,
+                bucket_endpoint,
+                bucket_key,
+                bucket_secret,
+                upload,
+            )
             with open(forecast_json_filename, "w", encoding="utf-8") as fp:
                 json.dump(data, fp, indent=4)
-            upload_to_bucket(forecast_json_filename, forecast_json_bucket_path,
-                             bucket_endpoint, bucket_key, bucket_secret)
+            upload_to_bucket(
+                forecast_json_filename,
+                forecast_json_bucket_path,
+                bucket_endpoint,
+                bucket_key,
+                bucket_secret,
+            )
             try:
                 os.remove(nc_filename)
                 os.remove(cog_filename)
